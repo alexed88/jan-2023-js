f2899abf4a162206492b2a5fc588307e3d51c017
alexed88 <domashalex0589@gmail.com>
2023-02-01 20:25:33 +0100

    innit

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,47 @@
+let a='hello'
+console.log(a)
+let b='owu'
+console.log(b)
+let c='com'
+console.log(c)
+let d='ua'
+console.log(d)
+let e=1
+console.log(e)
+let f=10
+console.log(f)
+let g=-999
+console.log(g)
+let h=123
+console.log(h)
+let i=3.14
+console.log(i)
+let j=2.7
+console.log(j)
+let k=16
+console.log(k)
+let l=true
+console.log(l)
+let m=false
+console.log(m)
+
+let fisrtName='Oleksandr'
+let middleName='Mykhailovych'
+let lastName='Domash'
+let pers=fisrtName+' '+middleName+' '+lastName
+console.log(pers)
+
+let n=100
+console.log(typeof n)
+let o='100'
+console.log(typeof o)
+let p=true
+console.log(typeof p)
+
+let firstNames = prompt('enter firstNames');
+firstNames='Sanja'
+let middleNames = prompt('enter middleNames');
+middleNames='Volodymyrovych'
+let age = prompt('enter age');
+let person=firstNames+' '+middleNames+' '+age;
+console.log(person)
